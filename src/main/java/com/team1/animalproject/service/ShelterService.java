f8c88397e1c21a5c890196d743984757e3758435
Muammer <musahin@netas.com.tr>
2019-12-17 20:33:33 +0300

    OK

--- src/main/java/com/team1/animalproject/service/ShelterService.java
+++ src/main/java/com/team1/animalproject/service/ShelterService.java
@@ -1,9 +1,7 @@
 package com.team1.animalproject.service;
 
 import com.team1.animalproject.model.Shelter;
-import com.team1.animalproject.model.Vet;
 import com.team1.animalproject.repository.ShelterRepository;
-import com.team1.animalproject.repository.VetRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
